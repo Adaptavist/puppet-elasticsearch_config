2066f1481621497505c86e7f314035e786af5bee
Matthew Hope <mhope@adaptavist.com>
2015-12-29 16:30:12 +0000

    spec test fixes

--- spec/classes/elasticsearch_config_spec.rb
+++ spec/classes/elasticsearch_config_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'elasticsearch_config', :type => 'class' do
     
   context "Should install elasticsearch" do
-    let(:facts) { { :host => Hash.new, :kernel => 'Linux', :osfamily => 'Debian', :operatingsystem => 'Debian' } }
+    let(:facts) { { :host => Hash.new, :kernel => 'Linux', :osfamily => 'Debian', :lsbdistid => 'Debian', :operatingsystem => 'Debian' } }
     it do
       should contain_class('elasticsearch')
       should contain_class('oracle_java')
